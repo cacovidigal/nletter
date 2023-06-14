93f69c0ef561130728e2136fcb1173e166963111
cacovidigal <marcosmsvidigal@gmail.com>
2023-06-13 21:22:34 -0300

    pensando na ligica js

--- mobile.js
+++ mobile.js
@@ -1,4 +1,12 @@
+
+
 function clicar(){
     var a = window.document.getElementById('subscribe')
-    a.innerHTML = 'pagina de sucesso!'//preciso descobrir como lincar a pagina de sucesso nesse botão
+    //ativa a pagina de sucesso mobile nesse botão
+    a.innerHTML = 'pagina de sucesso!'
+    //botão que retorna para a pagina inicial
+    //botão que ativa a pagina de sucesso desktop
+    //botão que retorna para pagina inicial
+    //mensagem em vermelho para incerir dados validos na caixa de e-mail
+
 }
